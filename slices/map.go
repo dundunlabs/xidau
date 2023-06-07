fa240e09008183bc132b456edfbcb853762fa538
Dung Tran <dung28.td@gmail.com>
2023-06-07 23:25:25 +0700

    bugfix

--- slices/map.go
+++ slices/map.go
@@ -2,8 +2,8 @@ package slices
 
 func Map[E1 any, E2 any](s []E1, f func(E1) E2) []E2 {
 	result := make([]E2, len(s))
-	for _, e := range s {
-		result = append(result, f(e))
+	for i, e := range s {
+		result[i] = f(e)
 	}
 	return result
 }
